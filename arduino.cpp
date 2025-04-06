54ace24dd3eaf61b2155480b6890185d503cc374
Gabriel Xavier Cardoso <gabrxavier353@gmail.com>
2025-04-05 22:25:26 -0300

    first commit

---
+++ arduino.cpp
@@ -0,0 +1,64 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(32, 16, 2);
+
+const int TEMP = A0;
+const int analogInPin = A2;
+const int analogOutPin = 8;
+const int SOIL = A1;
+
+int valTemp = 0, valSoil = 0;
+int celsius = 0;
+int umidade = 0;
+
+int sensorValue = 0;
+int outputValue = 0;
+bool ledState = false;
+
+void setup() {
+    lcd.init();
+    lcd.backlight();
+    lcd.clear();
+    
+    Serial.begin(9600);
+  
+    pinMode(analogOutPin, OUTPUT);
+    pinMode(analogInPin, INPUT);
+}
+
+void loop() {
+    // Leitura da temperatura
+    valTemp = analogRead(TEMP);
+    celsius = map(((valTemp - 20) * 3.04), 0, 1023, -40, 125);
+
+    // Leitura da luminosidade
+    sensorValue = analogRead(analogInPin);
+    outputValue = map(sensorValue, 0, 1023, 0, 255);
+    
+    if (sensorValue > 500) {
+        analogWrite(analogOutPin, outputValue);
+        ledState = true;  // LED aceso
+    } else {
+        analogWrite(analogOutPin, 0);
+        ledState = false; // LED apagado
+    }
+
+    // Leitura da umidade do solo
+    valSoil = analogRead(SOIL);
+    umidade = map(valSoil, 880, 0, 100, 0);
+
+    // Exibir no LCD
+    lcd.clear();
+    
+    lcd.setCursor(0, 0);
+    lcd.print("TEMP: "); lcd.print(celsius); lcd.print("C ");
+    
+    lcd.setCursor(10, 0);
+    lcd.print("U: "); lcd.print(umidade); lcd.print("%");
+    
+    lcd.setCursor(0, 1);
+    lcd.print("LED: "); lcd.print(ledState ? "ON " : "OFF");
+
+    delay(1000);
+}
